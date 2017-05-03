e34d80c6e11fdf32878209be31803377c5cc5a20
MimiManna <laxmi.manna@capgemini.com>
2017-05-03 10:11:40 +0200

    More comments

--- HotelApp/src/main/resources/public/js/alert.js
+++ HotelApp/src/main/resources/public/js/alert.js
@@ -7,3 +7,5 @@ $("#clickme").click(function(){alert("helleu");});
 // je kunt functies schrijven binnenin een methode
 // vergeet de jquery lib niet te importeren!
 // jQuery wordt gebruikt om de DOM tree te manipuleren
+
+// jquery.min = They are both the same functionally but the .min one has all unnecessary characters removed in order to make the file size smaller.
