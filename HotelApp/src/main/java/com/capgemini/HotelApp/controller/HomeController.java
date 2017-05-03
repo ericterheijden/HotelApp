4b9740d7bb8f337e544788b8c2c43a81c4ee8598
MimiManna <laxmi.manna@capgemini.com>
2017-05-03 09:41:43 +0200

    More comments on jQuery and the controller

--- HotelApp/src/main/java/com/capgemini/HotelApp/controller/HomeController.java
+++ HotelApp/src/main/java/com/capgemini/HotelApp/controller/HomeController.java
@@ -9,13 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // een @ staat voor annotatie (ook @Override)
 // het is een referentie naar een stukje code
+// Wat het eigenlijk doet is kijken of er restcontrollers zijn
+// Zo ja, dan gaat het NIET naar de resources map
+// En zoekt naar een RequestMapping methode
+// Zit er geen RequestMapping in (wat niet vaak voorkomt) dan alsnog naar public
 
 // controller is in staat data te returneren naar de browser
-// Naam van de controller kan misschien worden veranderd naar iets relevanters 
+// Naam van de controller kan misschien worden veranderd naar iets relevanters
 
 
 public class HomeController {
     @RequestMapping("/")
+    //
     public String home (){
         return "Hello World";
     }
--- HotelApp/src/main/resources/public/js/alert.js
+++ HotelApp/src/main/resources/public/js/alert.js
@@ -1,3 +1,9 @@
 alert("Welkom bij onze hotel applicatie");
 
 $("#clickme").click(function(){alert("helleu");});
+
+// $ sign gebruik je altijd als je start
+// vergeet de aanhalingstekens niet en gebruik # voor een ID
+// je kunt functies schrijven binnenin een methode
+// vergeet de jquery lib niet te importeren!
+// jQuery wordt gebruikt om de DOM tree te manipuleren
