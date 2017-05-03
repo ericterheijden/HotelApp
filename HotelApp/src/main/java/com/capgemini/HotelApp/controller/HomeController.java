285d57b7c385b6a0d80b10568387b6ebf7740e84
MimiManna <laxmi.manna@capgemini.com>
2017-05-03 09:24:25 +0200

    Editing controller

--- HotelApp/src/main/java/com/capgemini/HotelApp/controller/HomeController.java
+++ HotelApp/src/main/java/com/capgemini/HotelApp/controller/HomeController.java
@@ -1,7 +1,17 @@
 package com.capgemini.HotelApp.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by LMANNA on 3-5-2017.
  */
+
+@RestController
+// is in staat data te returneren naar de browser
+
 public class HomeController {
+    @RequestMapping("/")
+    public String home (){
+        return "Hello World";
+    }
 }
