ffcc448b76ef883818b17c527df794902b0e4579
jaccoverduijn <jacco.verduijn@capgemini.com>
2017-05-03 11:20:10 +0200

    input and extra accepted

--- HotelApp/src/main/java/com/capgemini/HotelApp/controller/HomeController.java
+++ HotelApp/src/main/java/com/capgemini/HotelApp/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.capgemini.HotelApp.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.capgemini.HotelApp.model.Room;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by LMANNA on 3-5-2017.
  */
 
-@RestController
+
 // een @ staat voor annotatie (ook @Override)
 // het is een referentie naar een stukje code
 // Wat het eigenlijk doet is kijken of er restcontrollers zijn
@@ -17,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 // controller is in staat data te returneren naar de browser
 // Naam van de controller kan misschien worden veranderd naar iets relevanters
 
-
+@RestController
 public class HomeController {
     @RequestMapping(value="/rooms", method= RequestMethod.GET)
-       public Room room(@PathVariable()){
+       public Room room(@RequestParam int input, @RequestParam String extra){
         Room room = new Room();
+        room.setNumber(input);
+        room.setType(extra);
+        return room;
     }
 }
---
+++ HotelApp/src/main/java/com/capgemini/HotelApp/model/Room.java
@@ -0,0 +1,37 @@
+package com.capgemini.HotelApp.model;
+
+/**
+ * Created by JVERDUIJ on 3-5-2017.
+ */
+public class Room {
+
+    private int number;
+    private String type;
+    private double price;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+}
